52aa16d3b82966483bc123237641133bb263ce63
Aneesh Dogra <lionaneesh@gmail.com>
2012-12-03 10:48:13 +0530

    run.py: pep8 cleanup.

--- run.py
+++ run.py
@@ -1,34 +1,37 @@
 #! /usr/bin/env python
 """Skeleton project file mainloop for new OLPCGames users"""
-import olpcgames, pygame, logging 
+import olpcgames
+import pygame
+import logging
 from olpcgames import pausescreen
 
-log = logging.getLogger( 'conozco-uruguay run' )
-log.setLevel( logging.DEBUG )
+log = logging.getLogger('conozco-uruguay run')
+log.setLevel(logging.DEBUG)
+
 
 def main():
     """The mainloop which is specified in the activity.py file
-    
     "main" is the assumed function name
     """
-    size = (800,600)
+    size = (800, 600)
     if olpcgames.ACTIVITY:
         size = olpcgames.ACTIVITY.game_size
     screen = pygame.display.set_mode(size)
-    
+
     clock = pygame.time.Clock()
 
     running = True
     while running:
-        screen.fill( (0,0,128))
-        milliseconds = clock.tick(25) # maximum number of frames per second
-        
-        # Event-management loop with support for pausing after X seconds (20 here)
+        screen.fill((0, 0, 128))
+        milliseconds = clock.tick(25)  # maximum number of frames per second
+
+        # Event-management loop with support for pausing after X seconds
+        # (20 here)
         events = pausescreen.get_events()
         # Now the main event-processing loop
         if events:
             for event in events:
-                log.debug( "Event: %s", event )
+                log.debug("Event: %s", event)
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.KEYDOWN:
