b6682df36f41923954d3f19fa3b3c7ed543b004e
Aneesh Dogra <lionaneesh@gmail.com>
2012-12-03 10:05:59 +0530

    creditos: pep8 cleanup.

--- datos/creditos.py
+++ datos/creditos.py
@@ -6,4 +6,4 @@ _("References: Gabriel Eirea (geirea@gmail.com)"),
 _("Please send comments and suggestions."),
 _("Sounds freesound.org downloaded from:"),
 _("btn045.wav courtesy of junggle"),
-_("boom1.wav courtesy of NoiseCollector") )
+_("boom1.wav courtesy of NoiseCollector"))
