765d8e2e8e196ddbd7148b0fe170bde8d6291826
Alan Aguiar <alanjas@hotmail.com>
2012-12-18 05:36:37 -0200

    update all with info of conozco america

--- datos/presentacion.py
+++
@@ -1,6 +0,0 @@
-from gettext import gettext as _
-
-PRESENT_ACTIONS = [
-_("What a beautiful world! I will explore that territory. There I go!"),
-_("Oh, no! The ship has problems, I'm going to crash!"),
-_("Luckily I was saved! Now I have to rebuild my ship. Need help!")]
---
+++ recursos/comun/datos/commons.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+ACTIVITY_NAME = _('I know Peru')
+
+PRESENTATION = [
+    _("What a beautiful world!\nI will explore that territory.\nThere I go!"),
+    _("Oh, no!\nThe ship has problems,\nI'm going to crash!"),
+    _("Luckily I was saved!\nNow I have to rebuild my ship.\nNeed help!")
+]
+
+PREFIX = [
+    _('We have to find'),
+    _('Now we have to find'),
+    _('We need to find'),
+    _('We have to look\na piece of my ship in'),
+    _("There's a piece\nof my ship in"),
+    _('According to my information\nhave to look at'),
+    _('We have to\ngo up'),
+    _('A piece of my\nship fell into')
+]
+
+SUFIX = [
+    _('Can you tell me where it is?'),
+    _('Where is it?'),
+    _('Can you show me where it is?'),
+    _('Can you find it?'),
+    _('Are we going up there?')
+]
+
+CORRECT = [
+    _('Very well!'),
+    _('Brilliant!'),
+    _('You found it!'),
+    _('Yes!')
+]
+
+WRONG = [
+    _('No, that is not correct'),
+    _('No, it is not there'),
+    _('No, that seems to be wrong'),
+    _('That is not correct'),
+    _('No,  try again'),
+    _('Seems not here'),
+    _('Keep trying...')
+]
+
+BYE = [
+    _('Now I can return\nFarewell to my planet.\nThanks for your help!'),
+    _('My ship is ready.\nBye and thanks for helping!'),
+    _("We did it!\nI'm Ready to take off.\nUntil next time!")
+]
+
+CREDITS = [
+    _("Author: Ivan Estremadoyro (ivanchhh@gmail.com)"),
+    _('Thanks to: Aneesh Dogra'),
+    _("Por favor enviar comentarios y sugerencias"),
+    "",
+    _("Este programa es software libre y desarrollado por la comunidad"),
+    _("Por más información visitá http://ceibaljam.org"),
+    "",
+    _("Based on Conozco Uruguay of Gabriel Eirea"),
+    _("Sonidos bajados de freesound.org: btn045.wav por junggle y boom1.wav por de NoiseCollector"),
+    _("Fuentes bajadas de urbanfonts.com: AllCaps.ttf por BobFrantic y Share-Regular.ttf por anónimo")
+]
+
