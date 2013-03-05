0c38539dc47b701de6c72b5f53568431fec6e77d
Alan Aguiar <alanjas@hotmail.com>
2013-03-05 00:58:06 -0200

    add rivers hack

--- conozcouy.py
+++ conozcouy.py
@@ -489,6 +489,9 @@ class ConozcoUy():
                             elif (index == 4):
                                 tipo = 3
                                 texto = _('the %s') % respuesta
+                            elif (index == 15):
+                                tipo = 3
+                                texto = _('the %(river)s') % {'river': respuesta}
                             elif (index == 5):
                                 tipo = 6
                                 texto = _('the %(route)s') % {'route': respuesta}
